6f845b1e747edb2de5779c9c886e64d81d22e3f4
Nathan Brickett <natey37@gmail.com>
2019-11-13 19:26:41 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,6 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizz_buzz(number)
+def fizzbuzz(number)
   if number % 3 == 0 && number % 5 == 0 
     return "FizzBuzz"
   elsif number % 5 == 0 
